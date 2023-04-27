d1c98d62931881666e97d67302fb1daa31b5c2a3
Andres Villca <andres24vt@gmail.com>
2023-04-27 17:38:11 +0200

    m

--- browsergameApp/models.py
+++ browsergameApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class userRols(models.Model):
+    idUser= models.ForeignKey(User, on_delete=models.CASCADE)
+    rold= models.ImageField()
+
+class players:
+    name= models.ForeignKey(User.name)
